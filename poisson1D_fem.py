4c6b68f6b7fe4d775f5f53d734e064c7409e74ea
Alessandro Andrea Zecchi <44672687+AlessandroZ94@users.noreply.github.com>
2024-12-08 17:51:41 +0100

    Update and rename fem.py to poisson1D_fem.py

--- poisson1D_fem.py
+++ poisson1D_fem.py
@@ -1,19 +1,20 @@
-# Problem: -u(x)'' = f(x) on O=[0,1]
-# f(x) = 2
-# BC: u[0]=u[1]=0
+# Problem: -u(x)'' = f on O=[0,1]
+# f = 2
+# BC, Dirichlet: u[0]=u[1]=0
 # weak formulation: integral(u'v') = integral(2*v)
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+def tridiag(a, b, c, k1=-1, k2=0, k3=1):
+    return np.diag(a, k1) + np.diag(b, k2) + np.diag(c, k3)
+    
 start = 0
 end = 1
-space = end - start
+omega = end - start
 num = 10
 x = np.linspace(start, end, num)
-f = 2 * np.sin(x)
-#f = 2
+f = 2 
 v = []
 
 for i in range(num):
@@ -25,29 +26,19 @@ v = np.asarray(v)
 for i in range(num):
     plt.plot(x,v[i])
 plt.show()
-b = np.empty([(num-2), 1])
+b =  np.full((num-2,1),f * omega/((num-1)))
 a = np.zeros([(num-2), (num-2)])
-for i in range(0, (num-2)):
-    c = (1/2*(f[i]+f[i-1])+1/2*(f[i+1]+f[i]))/2 * space/((num-1))
-    #c = f * space/((num-1))
-    b[i, 0] = c    
 
-for i in range(0, (num-2)):
-    for j in range(0, (num-2)):
-        if i==j:
-            a[i, j] = (num-1)*2
-        if abs(i-j) == 1:
-            a[i, j] = -(num-1)
+
+lower_upper_diag = np.full((num-3,1),-(num-1))
+main_diag = np.full((num-2,1), (num-1)*2)
+A = tridiag(lower_upper_diag, main_diag, lower_upper_diag)
 
 print(a)
 r = np.linalg.solve(a,b)
 
 r = np.insert(r, 0, 0)
 r = np.insert(r, r.size, 0)
-#print(r)
-#print(v)
-
-
 
 plt.plot(x,r)
 plt.show()
